728e2114429609b62ef7d78ee28eca72195502cb
Fouad ALLAOUI <fouadallaoui70@gmail.com>
2022-04-09 15:15:46 +0000

    FouadALLAOUI

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/documments', function () {
+    return 'documments';
+});
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });

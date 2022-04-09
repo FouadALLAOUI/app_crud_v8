856a46b070208b380719279d7a35ba5894e3930a
Fouad ALLAOUI <fouadallaoui70@gmail.com>
2022-04-09 18:25:03 +0000

    Correction

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
     public function register(Request $request){
         $fields = $request->validate([
             'name' => 'required|string',
-            'email' => 'required|string|unique:user,email',
+            'email' => 'required|string|unique:users,email',
             'password' => 'required|string|confirmed'
         ]);
 

e30477b42e4c0ff30794bb4c4593aa60819fae51
Fouad ALLAOUI <fouadallaoui70@gmail.com>
2022-04-09 17:52:54 +0000

    FouadALLAOUI

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use  App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+
+    }
+
+
+
+}
